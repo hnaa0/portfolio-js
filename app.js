ee78c141a8631f529fce8a2661d8272b2695cc04
hnaa0 <hnaa12@daum.net>
2023-01-30 14:15:02 +0900

    progress bar 추가

--- app.js
+++ app.js
@@ -1,19 +1,43 @@
 const $title = document.querySelector(".nav-title");
 const $nav_section = document.querySelectorAll(".nav-item");
+const $progress_nar = document.querySelector(".progress-bar");
 
-const ScrollMoveTop = () => {
+let timerID;
+
+const scrollMoveTop = () => {
   window.scrollTo({ top: 0, behavior: "smooth" });
 };
 
-const ScrollMoveSection = (key) => {
+const scrollMoveSection = (key) => {
   document.querySelector(key).scrollIntoView({ behavior: "smooth" });
 };
 
-$title.addEventListener("click", ScrollMoveTop);
+const throttle = (callback, time) => {
+  if (timerID) return;
+  timerID = setTimeout(() => {
+    callback();
+    timerID = undefined;
+  }, time);
+};
+
+const onScroll = () => {
+  const scrollTop = document.documentElement.scrollTop;
+  const height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  const scrollWidth = (scrollTop / height) * 100;
+  $progress_nar.style.width = scrollWidth + "%";
+};
+
+window.addEventListener("scroll", () => {
+  throttle(onScroll, 100);
+});
+
+$title.addEventListener("click", scrollMoveTop);
 
 $nav_section.forEach((navItem) => {
   navItem.addEventListener("click", (e) => {
     let dataKey = `.${e.target.dataset.key}`;
-    ScrollMoveSection(dataKey);
+    scrollMoveSection(dataKey);
   });
 });
