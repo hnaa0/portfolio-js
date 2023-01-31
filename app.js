e4c75e7277df0150f84a8301136f6abeeb83344b
hnaa0 <hnaa12@daum.net>
2023-01-31 15:24:14 +0900

    모달창 구현 및 css 파일 분리(main -> reset, main, responsive)

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-const $title = document.querySelector(".nav-title");
+const $onTopBtn = document.querySelector(".on-top-btn");
 const $nav_section = document.querySelectorAll(".nav-item");
 const $progress_nar = document.querySelector(".progress-bar");
+const $modalContainer = document.getElementsByClassName("modal-container");
+const $infoBtn = document.getElementsByClassName("project-info-btn");
+const $closeBtn = document.getElementsByClassName("close-btn");
 
 let timerID;
 
 const scrollMoveTop = () => {
-  window.scrollTo({ top: 0, behavior: "smooth" });
+  window.scrollTo({ top: "0", behavior: "smooth" });
 };
 
 const scrollMoveSection = (key) => {
@@ -29,11 +32,26 @@ const onScroll = () => {
   $progress_nar.style.width = scrollWidth + "%";
 };
 
+const modal = (num) => {
+  $infoBtn[num].addEventListener("click", () => {
+    $modalContainer[num].style.display = "flex";
+    document.body.style.overflow = "hidden";
+  });
+  $closeBtn[num].addEventListener("click", () => {
+    $modalContainer[num].style.display = "none";
+    document.body.style.overflow = "unset";
+  });
+};
+
+for (let i = 0; i < $closeBtn.length; i++) {
+  modal(i);
+}
+
 window.addEventListener("scroll", () => {
   throttle(onScroll, 100);
 });
 
-$title.addEventListener("click", scrollMoveTop);
+$onTopBtn.addEventListener("click", scrollMoveTop);
 
 $nav_section.forEach((navItem) => {
   navItem.addEventListener("click", (e) => {
